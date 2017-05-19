b8820c4773d981e38bd04582adbd8ac0c695a6e7
arno <liuxiaobing_09@126.com>
2017-05-19 09:32:12 +0000

    add the tested code

--- algorithm/smote.py
+++ algorithm/smote.py
@@ -1,35 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import imp
 import random
-from sklearn.neighbors import NearestNeighbors
+import pandas
 import numpy as np
+from sklearn.neighbors import NearestNeighbors
+#imp.reload(sys)
+#sys.setdefaultencoding('utf-8')
+
 class Smote:
     def __init__(self,samples,N=10,k=5):
-        self.n_samples,self.n_attrs=samples.shape
+        self.n_samples, self.n_attrs=samples.shape
         self.N=N
         self.k=k
         self.samples=samples
-        self.newindex=0
-       # self.synthetic=np.zeros((self.n_samples*N,self.n_attrs))
 
     def over_sampling(self):
-        N=int(self.N/100)
-        self.synthetic = np.zeros((self.n_samples * N, self.n_attrs))
+        keep_list=[]
+        if self.N<100:
+            old_n_samples=self.n_samples
+            #print("old_n_samples:", old_n_samples)
+            self.n_samples=int(float(self.N)/100*old_n_samples)
+            #print("n_samples:", self.n_samples)
+            keep=np.random.permutation(old_n_samples)[:self.n_samples]
+            #print("keep:", keep)
+            #change the delimiter from space to comma,why keep's elements are delimited by comma, fixme
+            for item in keep:
+                keep_list.append(item)
+            #print("keep:", keep_list)
+            new_samples=self.samples.iloc[keep_list]
+            self.samples=new_samples
+            self.N=100
+
+        N=int(self.N/100) #the number of new synthetic minority class samples
+        self.synthetic=np.zeros((self.n_samples*N, self.n_attrs))
+        self.new_index=0
+
         neighbors=NearestNeighbors(n_neighbors=self.k).fit(self.samples)
-        print 'neighbors',neighbors
+        #print("neighbors:", neighbors)
+        arr_samples=np.array(self.samples)
         for i in range(len(self.samples)):
-            nnarray=neighbors.kneighbors(self.samples[i].reshape(1,-1),return_distance=False)[0]
-            #print nnarray
-            self._populate(N,i,nnarray)
+            nnarray=neighbors.kneighbors(arr_samples,return_distance=False)[0]
+            #store the k-nearest neighbors's index
+            self.__populate(N, i, nnarray )
+
         return self.synthetic
 
+    #extract n times randomly from the k-nearest neighbors, generating n synthetic samples
+    def __populate(self, N, i, nnarray):
+        for i in range(N):
+            nn = np.random.randint(0, self.k)
+            print(nnarray[nn])
+            dif=self.samples.iloc[nnarray[nn]]-self.samples.iloc[i]    #including the class label
+            gap=np.random.rand(1,self.n_attrs)
+            self.synthetic[self.new_index]=self.samples.iloc[i]+gap.flatten()*dif
+            self.new_index+=1
 
-    # for each minority class samples,choose N of the k nearest neighbors and generate N synthetic samples.
-    def _populate(self,N,i,nnarray):
-        for j in range(N):
-            nn=random.randint(0,self.k-1)
-            dif=self.samples[nnarray[nn]]-self.samples[i]
-            gap=random.random()
-            self.synthetic[self.newindex]=self.samples[i]+gap*dif
-            self.newindex+=1
-a=np.array([[1,2,3],[4,5,6],[2,3,1],[2,1,2],[2,3,4],[2,3,4]])
-s=Smote(a,N=100)
-print s.over_sampling()
+if __name__ == "__main__":
+    #file_fullpath='/home/login01/Workspaces/python/dataset/module_data_stg1/sample'
+    file_fullpath='/home/login01/Workspaces/python/dataset/cs.csv'
+    cs=pandas.read_csv(file_fullpath,sep=',',index_col=0,na_values='NA',low_memory=False)
+    cs_mean_MonthlyIncome = cs.MonthlyIncome.mean(skipna=True)                                          
+    cs_mean_NumberOfDependents = cs.NumberOfDependents.mean(skipna=True)
+    cs.ix[:, 'MonthlyIncome'] = cs.MonthlyIncome.fillna(cs_mean_MonthlyIncome, inplace=False)
+    cs.ix[:, 'NumberOfDependents'] = cs.NumberOfDependents.fillna(cs_mean_NumberOfDependents, inplace=False)
+    ismote=Smote(cs,20,6)
+    print(ismote.n_samples)
+    print(ismote.n_attrs)
+    mysample=ismote.over_sampling()
+    print(mysample)
