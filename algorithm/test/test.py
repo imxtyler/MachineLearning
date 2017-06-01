401924247b98142a5d1020665ae7a9eadfd27053
arno <liuxiaobing_09@126.com>
2017-06-01 11:05:11 +0800

    Add directory test.

---
+++ algorithm/test/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os
+
+if __name__ == "__main__":
+    area_dict = {}
+    resource_dir = '../resources'
+    for dirpath,dirnames,filenames in os.walk(resource_dir):
+        for file in filenames:
+            filepath = dirpath+'/'+file
+            res_file = open(filepath,'r')
+            while True:
+                line = res_file.readline()
+                if not line:
+                    break
+                area_number,area = line.strip().split(',')
+                area_dict.setdefault(area,area_number)
+            for key,val in area_dict.items():
+                print(str(key)+','+str(val))
