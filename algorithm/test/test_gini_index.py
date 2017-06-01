da39c0245f04584c519073bfbc92e30fc9e4e568
Arno <liuxiaobing_09@126.com>
2017-06-01 11:26:05 +0800

    Add file test_gini_index.py, test_random_sampling.py, test_smote.py, etc.

---
+++ algorithm/test/test_gini_index.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import pandas
+import gini_index
+from gini_index import GiniIndex
+
+if __name__ == "__main__":
+    file_fullpath = '/home/login01/Workspaces/python/dataset/cs.csv'
+    #df = pandas.read_csv(file_fullpath,sep=',',index_col=0,na_values='NA',dtype=object,low_memory=False)
+    #df = pandas.read_csv(file_fullpath,sep=',',index_col=0,na_values='NA',low_memory=False)
+    df = pandas.read_csv(file_fullpath,sep=',',na_values='NA',low_memory=False)
+    attribute=["RevolvingUtilizationOfUnsecuredLines","age","NumberOfTime30-59DaysPastDueNotWorse","DebtRatio","MonthlyIncome","NumberOfOpenCreditLinesAndLoans","NumberOfTimes90DaysLate","NumberRealEstateLoansOrLines","NumberOfTime60-89DaysPastDueNotWorse","NumberOfDependents"]
+    target_key = "SeriousDlqin2yrs"
+    df[target_key] = df[target_key].fillna(0)
+    target = df[target_key]
+    #pandas.set_option('display.max_rows', None)
+    #print(target)
+    gini = GiniIndex(df,attribute,target_key,target)
+    #mygini_index = gini.gini_index()
+    #print("mygini_score:", mygini_index)
+    mygini_index_dict = gini.gini_index()
+    gini_list = sorted(mygini_index_dict.items(),key=lambda item:item[1])
+    print("Gini index of each attribute:")
+    #for key,val in mygini_index_dict.items():
+    #    print("%s:%s" % (key,val))
+    for item in gini_list:
+        print(item)
---
+++ algorithm/test/test_random_sampling.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import random_sampling
+
+if __name__ == "__main__":
+    #print(extend_weight_random(['A','B','C','D'],[5,2,2,1]))
+    #print(weight_random(['A','B','C','D'],[5,2,2,1]))
+    print(random_sampling.sort_weight_random(['A','B','C','D'],[5,2,2,1]))
