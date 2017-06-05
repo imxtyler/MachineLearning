47f9c54052fd95d56cd79f5b9f3fbb88070627c7
Arno <liuxiaobing_09@126.com>
2017-06-05 18:31:11 +0800

    Change the logic of class DataPreprocessing.

--- algorithm/utils/preprocessing.py
+++ algorithm/utils/preprocessing.py
@@ -9,7 +9,7 @@ class DataPreprocessing():
     def __init__(self,df,attributes,key):
         '''
         :param df: the dataframe containing the attriute and target
-        :param attributes: the attributes that need to be calculated the gini score and is type 1
+        :param attributes: the X's attributes that need to be calculated the gini score and is type 1
         :param key: the target's label in the classification problem
         '''
         self.sample_num,self.attr_num = df.shape
@@ -20,47 +20,60 @@ class DataPreprocessing():
     def data_summary(self):
         print("number of attributes:%d, number of samples:%d" % (self.attr_num,self.sample_num))
         print(self.df.info())
-        print(self.df.describe())
-        print("proportion of null values of each attribute:")
+        #print(self.df.describe())
+        print("ratio of null values of each attribute:")
         print(self.df.isnull().sum()/self.sample_num)
         print("targe key, size of non-null values:",self.df.groupby(self.key).size())
+        print("targe key, size of negetive values:",(self.sample_num-self.df[self.key].sum()))
+        print("targe key, size of positive values:",self.df[self.key].sum())
+        print("targe key, rate of positive values:",(self.df[self.key].sum()/self.sample_num))
 
-    def set_missing_label(self,numberical_attributes,label):
-        '''
-        :param numberical_attributes: the numberical attributes that need to be calculated
-        :param label: the label containing null value
-        '''
-        # Put all the numberical features into Random Forest Regressor
-        label_df = self.df[numberical_attributes]
+    #def set_x_missing_label(self,numberical_attributes,label):
+    #    '''
+    #    :param numberical_attributes: the numberical attributes that need to be calculated
+    #    :param label: the label containing null value
+    #    '''
+    #    # Put all the numberical features into Random Forest Regressor
+    #    label_df = self.df[numberical_attributes]
 
-        # Divide the customers into two parts: who's label is known and unknown
-        known_label = label_df[label_df[label].notnull()].as_matrix()
-        unknown_label = label_df[label_df[label].isnull()].as_matrix()
+    #    # Divide the customers into two parts: who's label is known and unknown
+    #    known_label = label_df[label_df[label].notnull()].as_matrix()
+    #    unknown_label = label_df[label_df[label].isnull()].as_matrix()
 
-        # Target label
-        label_y = known_label[:, 0]
+    #    # Target label
+    #    label_y = known_label[:, 0]
 
-        # X are feature attributes
-        label_X = known_label[:, 1:]
+    #    # X are feature attributes
+    #    label_X = known_label[:, 1:]
 
-        rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
-        rfr.fit(label_X, label_y)
-        predictedlabels = rfr.predict(unknown_label[:, 1::])
+    #    rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
+    #    rfr.fit(label_X, label_y)
+    #    predictedlabels = rfr.predict(unknown_label[:, 1::])
 
-        # Fill the null value via the predict labels
-        self.df.loc[(self.df.label.isnull()),label] = predictedlabels
+    #    # Fill the null value via the predict labels
+    #    self.df.loc[(self.df.label.isnull()),label] = predictedlabels
 
-        return self.df,rfr
+    #    return self.df,rfr
 
-    def set_missing_label_mean(self,attributes):
+    def set_x_missing_label_mean(self,attributes,allnull=False,nullvalue=-1):
         '''
         :param attributes: the attributes who's null value need to be filled with mean value
+        :param allnull: bool, sign that if need to process the attribute whose value is all null
+        :param nullvalue: the value that using fill the attribute whose value is all null
         '''
         for attr in attributes:
             mean_attr = self.df[attr].mean(skipna=True)
-            #df[attr] = df[attr].fillna(mean_attr,inplace=True)
-            self.df.loc[(self.df[attr].isnull()),attr] = mean_attr
+            if allnull==True:
+                if (len(self.df[self.df[attr].isnull()])==self.sample_num):
+                    self.df.loc[(self.df[attr].isnull()),attr] = nullvalue
+                else:
+                    #df[attr] = df[attr].fillna(mean_attr,inplace=True)
+                    self.df.loc[(self.df[attr].isnull()),attr] = mean_attr
+            else:
+                self.df.loc[(self.df[attr].isnull()),attr] = mean_attr
 
+        if self.key in (list(self.df.columns.values)):
+            self.df.drop(self.key,axis=1,inplace=True)
         return self.df
 
     def filter_numberical_attr(self):
@@ -88,7 +101,7 @@ class DataPreprocessing():
             print(e)
             return False
 
-    def transform_dtype(self,attributes,d_type,uniform_type=False):
+    def transform_x_dtype(self,attributes,d_type,uniform_type=False):
         '''
         :param attributes: list, the attributes that need to be transformed its' dtype 
         :param d_type: list, the destination dtypes of attributes, the entry's value must be int32, int64, float32, float64, etc.
@@ -105,9 +118,11 @@ class DataPreprocessing():
         except Exception as e:
             print(e)
 
+        if self.key in (list(self.df.columns.values)):
+            self.df.drop(self.key,axis=1,inplace=True)
         return self.df
 
-    def transform_to_dummies(self,attributes):
+    def transform_x_to_dummies(self,attributes):
         '''
         :param attributes: the attributes that need to be transformed to dummies
         '''
@@ -116,9 +131,11 @@ class DataPreprocessing():
             dummies_item = pandas.get_dummies(self.df[item],prefix=item)
             dummies_attrs.append(dummies_item)
 
+        if self.key in (list(self.df.columns.values)):
+            self.df.drop(self.key,axis=1,inplace=True)
         return self.df,dummies_attrs
 
-    def transform_to_binary(self,attributes):
+    def transform_x_to_binary(self,attributes):
         '''
         :param attributes: the attributes that need to be transformed to binary 
         '''
@@ -128,6 +145,8 @@ class DataPreprocessing():
             self.df.loc[(self.df[item].isnull()),item] =0
             self.df.loc[(self.df[item].notnull()),item]=1
 
+        if self.key in (list(self.df.columns.values)):
+            self.df.drop(self.key,axis=1,inplace=True)
         return self.df
 
     def china_area_number_mapping(self,attributes,resource_dir):
@@ -155,6 +174,8 @@ class DataPreprocessing():
             self.df[attr] = self.df[attr].map(area_mapping)
 
         #return area_dict
+        if self.key in (list(self.df.columns.values)):
+            self.df.drop(self.key,axis=1,inplace=True)
         return self.df
 
     def single_attr_binning(self,attribute,bin_num=1,labels=None):
@@ -200,16 +221,20 @@ class DataPreprocessing():
         :param attributes: the attributes that need to transform to binning_rate 
         :param bin_num: the number of bins
         :param labels: the labels of bins
+        :param allnull: bool, sign that if need to process the attribute whose value is all null
+        :param nullvalue: the value that using fill the attribute whose value is all null
         :return: df 
         '''
         pass  # fixme
 
-    def dummies_and_fillna(self):
+    def x_dummies_and_fillna(self,allnull=False,nullvalue=-1):
         numberical_attrs,nonnumberical_attrs = self.filter_numberical_attr()
-        self.df,dummies_attrs = self.transform_to_dummies(nonnumberical_attrs)
+        self.df,dummies_attrs = self.transform_x_to_dummies(nonnumberical_attrs)
         for item in dummies_attrs:
             self.df = pandas.concat([self.df,item],axis=1)
         self.df.drop(nonnumberical_attrs,axis=1,inplace=True)
-        self.df = self.set_missing_label_mean(list(self.df.columns))
+        self.df = self.set_x_missing_label_mean(list(self.df.columns),allnull=allnull,nullvalue=nullvalue)
 
+        if self.key in (list(self.df.columns.values)):
+            self.df.drop(self.key,axis=1,inplace=True)
         return self.df
