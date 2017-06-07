ff786cb1d9b24673897bfc9d6232b4722652b61e
Arno <liuxiaobing_09@126.com>
2017-06-07 15:15:19 +0800

    Change the grammatical mistake.

--- algorithm/utils/unbalanced/ensemble.py
+++ algorithm/utils/unbalanced/ensemble.py
@@ -10,7 +10,7 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import confusion_matrix
 import logging
 
-class Ensemble():    
+class Ensemble():
     # In[] n_subset number of subset, version:'easy_ensemble' or 'balance_cascade'
     def __init__(self,version='easy_ensemble',random_seed=None,n_subset=10,n_tree=10):
         self.random_seed = random_seed
@@ -19,14 +19,14 @@ class Ensemble():
         self.ensemble = {}
         self.ensemble['adclf'] = []
         self.ensemble['bias'] = []
-        self.logger = logging.getLogger(__name__)
-        
-    # In[] find the class statistics before sampling
-    def fit(self,x,y):               
+        self.stats_c = {}
         self.min_c = None
         self.maj_c = None
-        self.stats_c = {}
         self.x_shape = None
+        self.logger = logging.getLogger(__name__)
+
+    # In[] find the class statistics before sampling
+    def fit(self,x,y):
         self.logger.info('compute classes statistics...')
         self.x_shape = x.shape
         self.stats_c = Counter(y)
@@ -34,14 +34,14 @@ class Ensemble():
         self.maj_c = max(self.stats_c,key=self.stats_c.get)
         self.logger.info('%s classes detected: %s'
                          %(len(self.stats_c), ','.join(str(self.stats_c))))
-    
+
     # In[] x,y: subset need to be sampled not all set
     def under_sample(self,x,y,num_samples):
         random_state = np.random.RandomState(self.random_seed)
         sel_id = random_state.choice(range(len(x)),size=num_samples)
         return x[sel_id],y[sel_id]
-    
-    # In[] tune bias to be false positive 
+
+    # In[] tune bias to be false positive
     def bias_tune(self,false_positive,adaclf,x,y):
         estimators = adaclf.estimators_
         weights = adaclf.estimator_weights_
@@ -53,9 +53,10 @@ class Ensemble():
         res = sorted(res,reversed=True)
         bias = res[round(false_positive*len(ind))]
         return bias
-        
+
     # In[]
     def sample(self,x,y):
+        self.fit(x,y)
         num_samples = self.stats_c[self.min_c]
         x_min = x[y==self.min_c]
         y_min = y[y==self.min_c]
@@ -68,12 +69,12 @@ class Ensemble():
             x_under_samp,y_under_sample = self.under_sample(x_maj,y_maj,num_samples)
             x_samp = np.vstack((x_sample,x_under_samp))
             y_samp = np.hstack((y_sample,y_under_sample))
-            samples.append((x_samp,y_samp))      
-        for x_samp,y_samp in samples:                 
+            samples.append((x_samp,y_samp))
+        for x_samp,y_samp in samples:
             adaboost_clf = AdaBoostClassifier(base_estimator='DecisionTreeClassifier',
                                               n_estimators=20)
-            adaboost_clf.fit(x_samp,y_samp)      
-            if self.version == 'easy_ensemble': 
+            adaboost_clf.fit(x_samp,y_samp)
+            if self.version == 'easy_ensemble':
                 self.ensemble['adclf'].append(adaboost_clf)
             else:
                 t = np.power((self.stats_c[self.min_c]*1.0/self.stats_c[self.maj_c]),
@@ -84,4 +85,4 @@ class Ensemble():
                 bias = self.bias_tune(t,adaboost_clf,x_samp,y_samp)
                 self.ensemble['adclf'].append(adaboost_clf)
                 self.ensemble['bias'].append(bias)
-        return self.ensemble                
+        return self.ensemble
