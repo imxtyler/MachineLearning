958480d829757cd6bbc35276f705d3051babc6e7
Arno <liuxiaobing_09@126.com>
2017-06-16 12:08:39 +0800

    Change the logic

--- algorithm/test/test_random_forest.py
+++ algorithm/test/test_random_forest.py
@@ -276,15 +276,15 @@ if __name__ == "__main__":
     pd.set_option('display.max_rows', None)
     attributes_dir = '../resources/attributes'
 
-    attribute_file_path = attributes_dir+'/'+'user_portrait_info_v1'
-    data_path= '/home/login01/Workspaces/python/dataset/module_data_stg2_tt'
+    #attribute_file_path = attributes_dir+'/'+'user_portrait_info_v1'
+    #data_path= '/home/login01/Workspaces/python/dataset/module_data_stg2_tt'
     #data_path= '/home/login01/Workspaces/python/dataset/module_data_stg2'
 
     #attribute_file_path = attributes_dir+'/'+'user_portrait_info_v1_20170608'
     #data_path= '/home/login01/Workspaces/python/dataset/module_data_20170608'
 
-    #attribute_file_path = attributes_dir+'/'+'user_portrait_info_v2_20170612'
-    #data_path= '/home/login01/Workspaces/python/dataset/module_data_stg2_20170612'
+    attribute_file_path = attributes_dir+'/'+'user_portrait_info_v2_20170612'
+    data_path= '/home/login01/Workspaces/python/dataset/module_data_stg2_20170612'
 
     attribute_file = open(attribute_file_path,'r')
     attributes = []
@@ -304,8 +304,8 @@ if __name__ == "__main__":
 
     to_binary_attrs = ['user_live_address','user_rela_name','user_relation','user_rela_phone','user_high_edu','user_company_name']
     area_attrs = ['user_live_province','user_live_city']
-    X_train,X_test,y_train,y_test = data_preprocess(data_path=data_path,form=1,attributes=attributes,target_key=target_key,to_binary_attrs=to_binary_attrs,area_attrs=area_attrs,show=False)
-    #X_train,X_test,y_train,y_test = data_preprocess(data_path=data_path,form=2,attributes=attributes,all_labels=all_labels,target_key=target_key,to_binary_attrs=to_binary_attrs,area_attrs=area_attrs,show=True,cut_point=6)
+    #X_train,X_test,y_train,y_test = data_preprocess(data_path=data_path,form=1,attributes=attributes,target_key=target_key,to_binary_attrs=to_binary_attrs,area_attrs=area_attrs,show=False)
+    X_train,X_test,y_train,y_test = data_preprocess(data_path=data_path,form=2,attributes=attributes,all_labels=all_labels,target_key=target_key,to_binary_attrs=to_binary_attrs,area_attrs=area_attrs,show=False,cut_point=6)
     #train_test(X_train,X_test,y_train,y_test)
     train_test1(X_train,X_test,y_train,y_test)
 
